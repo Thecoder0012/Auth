aefe3c6153e449c2339088a38e0ffafe8fa04c39
Thecoder0012 <mardineliassi57@gmail.com>
2023-05-02 02:36:24 +0200

    added useEffect hook and session gets deleted when logging out.

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import { Routes, Route} from "react-router-dom";
 import { Signup } from './components/Signup';
 import { Login } from "./components/Login";
 import { Homepage } from "./components/Homepage.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 function App() {
-  
+
   const[auth,setAuth] = useState(false);
+    useEffect(() => {
     axios.get("http://localhost:8080/login", {
         withCredentials: true,
-    }).then(result => setAuth(result.data.authenticated));
+    }).then(result => {
+      console.log(result.data);
+      setAuth(result.data.authenticated)});
+    },[])
 
   return (
    <div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
 import styles from '../styles/Homepage.module.css'
 export const Navbar = () => {
+
+    async function logout(){
+        await axios.get("http://localhost:8080/destroy", {
+         withCredentials: true,
+       });
+    }
   return (
     <div className={styles.container}>
       <ul>
         <li> <Link to="/homepage">Home</Link> </li>
-        <li> <Link to="/login">Logout</Link> </li>
+        <li> <Link onClick={logout} to="/login">Logout</Link> </li>
       </ul>
     </div>
   );
